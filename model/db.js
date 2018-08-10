1207a819f8269723a26d556807d55fbc3be899c6
paregos <paregos18@gmail.com>
2018-08-10 20:09:47 +1000

    Updated db.js to include environment variables

--- model/db.js
+++ model/db.js
@@ -2,10 +2,15 @@ const Sequelize = require('sequelize');
 
 const Card = require('./card');
 
-const sequelize = new Sequelize('', '', '', {
-  dialect: 'sqlite',
-  storage: 'db.sqlite',
-  logging: false
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_NAME, process.env.DB_NAME, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 const syncedPromise = sequelize
