89cc7188985e026fb7874420d6cd218ec2b7fd93
Hanzhi Wang <hanzhi2@gmail.com>
2018-08-12 15:43:07 +1200

    Fix missing card IDs

--- model/db.js
+++ model/db.js
@@ -1,33 +1,38 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const Card = require('./card');
+const Card = require("./card");
 
+// const sequelize = new Sequelize('', '', '', {
+//   dialect: 'sqlite',
+//   storage: 'db.sqlite',
+//   logging: false
+// });
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_NAME, process.env.DB_NAME, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 });
 
 const syncedPromise = sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Syncing Database...');
-    if(process.env.MTGA_RESET_TABLES == 'true') {
-      console.log('Rebuilding tables...');
-    }
-    return sequelize.sync({ force: process.env.MTGA_RESET_TABLES == 'true' })
-  })
-  .then(() => {
-    console.log('Database Synced');
-  });
+    .authenticate()
+    .then(() => {
+        console.log("Syncing Database...");
+        if (process.env.MTGA_RESET_TABLES == "true") {
+            console.log("Rebuilding tables...");
+        }
+        return sequelize.sync({ force: process.env.MTGA_RESET_TABLES == "true" });
+    })
+    .then(() => {
+        console.log("Database Synced");
+    });
 
 module.exports = {
-  sequelize,
-  syncedPromise,
-  Card: Card(sequelize, Sequelize)
-}
+    sequelize,
+    syncedPromise,
+    Card: Card(sequelize, Sequelize)
+};
--- scrapers/get-cards.js
+++ scrapers/get-cards.js
@@ -1,65 +1,96 @@
-const axios = require('axios');
-const fs = require('fs');
+const axios = require("axios");
+const fs = require("fs");
 
-const db = require('../model/db');
+const db = require("../model/db");
 
 function writeCards(cards) {
-  return Promise.all([
-    new Promise((resolve, reject) => {
-      // Write to json for use by the electron client
-      fs.writeFile('electron/cards.json', JSON.stringify(cards), err => {
-        if(err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      })
-    }),
+    return Promise.all([
+        db.Card.findAll().then((cardIds) => {
+            const cardMappings = {};
+            cardIds.forEach((card) => {
+                cardMappings[card.id] = {
+                    name: card.name,
+                    ...cards[card.name]
+                };
+            });
 
-    // Write to DB for use by the server
-    db.Card.destroy({ where: {} })
-      .then(() => {
-        return db.Card.bulkCreate(Object.keys(cards).map(key => {
-          return {
-            id: parseInt(key),
-            name: cards[key].name
-          }
-        }))
-      })
-  ]);
+            return new Promise((resolve, reject) => {
+                // Write to json for use by the electron client
+                fs.writeFile("electron/cards.json", JSON.stringify(cards), (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            });
+        }),
+
+        // Write to DB for use by the server
+        Promise.all(
+            Object.keys(cards).map((key) => {
+                return db.Card.update(
+                    {
+                        // Placeholder for when more properties are stored on the server DB later, add them here
+                        name: key
+                    },
+                    { where: { name: key } }
+                );
+            })
+        )
+    ]);
 }
 
 function getCards(url, cards, pageNumber) {
-  console.log(`Getting page ${pageNumber}...`);
-  return axios.get(url)
-    .then(res => {
-      cardList = res.data.data;
-      nextUrl = res.data.next_page;
-      cardList.forEach(card => {
-        // Double check that it has an ID set
-        // Some cards are in the list but dont have a MTGA ID
-        if(card.arena_id) {
-          cards[card.arena_id] = {
-            name: card.name,
-            cost: card.mana_cost,
-            colors: card.colors,
-            colorIdentity: card.color_identity
-          }
-        }
-      });
+    console.log(`Getting page ${pageNumber}...`);
+    return axios.get(url).then((res) => {
+        cardList = res.data.data;
+        nextUrl = res.data.next_page;
+        // Note: when a card appears in multiple sets, Scryfall doesn't seem to list an ID
+        cardList.forEach((card) => {
+            cards[card.name] = {
+                cost: card.mana_cost,
+                colors: card.colors,
+                colorIdentity: card.color_identity
+            };
+        });
 
-      if(nextUrl) {
-        return getCards(nextUrl, cards, pageNumber + 1);
-      } else {
-        return writeCards(cards);
-      }
-    })
+        if (nextUrl) {
+            return getCards(nextUrl, cards, pageNumber + 1);
+        } else {
+            return writeCards(cards);
+        }
+    });
 }
 
 function scrape() {
-  return db.syncedPromise.then(() => {
-    return getCards('https://api.scryfall.com/cards/search?q=game:arena', {}, 1);
-  });
+    return db.syncedPromise
+        .then(() => {
+            // Delete all card entries
+            return db.Card.destroy({ where: {} });
+        })
+        .then(() => {
+            // Get the complete list of MTGA card IDs from this guy's repo
+            return axios.get("https://raw.githubusercontent.com/Fugiman/deckmaster/master/client/types/cards.go");
+        })
+        .then((res) => {
+            // Build array of [id, name]
+            const cardIds = res.data.match(/Card{.+}/g).map((cardRow) => {
+                const cardInfo = cardRow.match(/".+?"/g);
+                const cardId = parseInt(cardInfo[0].slice(1, -1));
+                const cardName = cardInfo[1].slice(1, -1);
+                return {
+                    id: cardId,
+                    name: cardName
+                };
+            });
+
+            // Write mapping to DB
+            return db.Card.bulkCreate(cardIds);
+        })
+        .then(() => {
+            return getCards("https://api.scryfall.com/cards/search?q=game:arena", {}, 1);
+        });
 }
 
 module.exports = scrape;
